7ac4c51215a0cf6f19991a420598a3479ee08a3d
Newest-IT-Bro <bjncooper@gmail.com>
2026-04-14 11:46:03 +0100

    fourth commit

--- 3-s3-bucket.tf
+++ 3-s3-bucket.tf
@@ -23,10 +23,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "public_jenkins_en
 resource "aws_s3_bucket_public_access_block" "public_jenkins_pab" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "public_jenkins_lifecycle" {
@@ -54,27 +54,27 @@ resource "aws_s3_object" "Armageddon" {
 
 resource "aws_s3_object" "extension" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
-  key    = "extension.png"
-  source = "${path.module}/screenshots/extension.png"
-  content_type = "image/png"
+  key    = "extension.jpg"
+  source = "${path.module}/screenshots/extension.jpg"
+  content_type = "image/jpg"
 
-  etag   = filemd5("${path.module}/screenshots/extension.png")
+  etag   = filemd5("${path.module}/screenshots/extension.jpg")
 }
 
 resource "aws_s3_object" "webhook" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
-  key    = "webhook.png"
-  source = "${path.module}/screenshots/webhook.png"
-  content_type = "image/png"
+  key    = "webhook.jpg"
+  source = "${path.module}/screenshots/webhook.jpg"
+  content_type = "image/jpg"
 
-  etag   = filemd5("${path.module}/screenshots/webhook.png")
+  etag   = filemd5("${path.module}/screenshots/webhook.jpg")
 }
 
 resource "aws_s3_object" "s3jenkins" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
-  key    = "s3jenkins.png"
-  source = "${path.module}/screenshots/s3jenkins.png"
-  content_type = "image/png"
+  key    = "s3jenkins.jpg"
+  source = "${path.module}/screenshots/s3jenkins.jpg"
+  content_type = "image/jpg"
 
-  etag   = filemd5("${path.module}/screenshots/s3jenkins.png")
+  etag   = filemd5("${path.module}/screenshots/s3jenkins.jpg")
 }
