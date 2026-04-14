22f6018d149d8b8cb48ff24799f8e7232742fa6d
Newest-IT-Bro <bjncooper@gmail.com>
2026-04-14 20:43:06 +0100

    third commit

--- 3-s3-bucket.tf
+++ 3-s3-bucket.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "public_jenkins_bucket" {
-  bucket = "s3jenkins-bucket-tester"
-  force_destroy = true
+  bucket = "s3jenkins-bucket-public"
+  force_destroy = false
 }
 
 resource "aws_s3_bucket_versioning" "public_jenkins_versioning" {
@@ -10,39 +10,35 @@ resource "aws_s3_bucket_versioning" "public_jenkins_versioning" {
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "public_jenkins_encryption" {
+resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_public_access_block" "public_jenkins_pab" {
+resource "aws_s3_bucket_policy" "public_policy" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.public_jenkins_bucket.arn}/*"
+      }
+    ]
+  })
 }
-
-resource "aws_s3_bucket_lifecycle_configuration" "public_jenkins_lifecycle" {
-  bucket = aws_s3_bucket.public_jenkins_bucket.id
-
-  rule {
-    id     = "delete-old-versions"
-    status = "Enabled"
-
-    noncurrent_version_expiration {
-      noncurrent_days = 7
-    }
-  }
-}
-
-
 resource "aws_s3_object" "Armageddon" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
   key    = "Armageddon.md"
@@ -61,13 +57,22 @@ resource "aws_s3_object" "extension" {
   etag   = filemd5("${path.module}/screenshots/extension.jpg")
 }
 
-resource "aws_s3_object" "webhook" {
+resource "aws_s3_object" "Jenkins-Webhook" {
   bucket = aws_s3_bucket.public_jenkins_bucket.id
-  key    = "webhook.jpg"
-  source = "${path.module}/screenshots/webhook.jpg"
+  key    = "Jenkins-Webhook.jpg"
+  source = "${path.module}/screenshots/Jenkins-Webhook.jpg"
   content_type = "image/jpg"
 
-  etag   = filemd5("${path.module}/screenshots/webhook.jpg")
+  etag   = filemd5("${path.module}/screenshots/Jenkins-Webhook.jpg")
+}
+
+resource "aws_s3_object" "Jenkins-Pipeline" {
+  bucket = aws_s3_bucket.public_jenkins_bucket.id
+  key    = "Jenkins-Pipeline.jpg"
+  source = "${path.module}/screenshots/Jenkins-Pipeline.jpg"
+  content_type = "image/jpg"
+
+  etag   = filemd5("${path.module}/screenshots/Jenkins-Pipeline.jpg")
 }
 
 resource "aws_s3_object" "s3jenkins" {
